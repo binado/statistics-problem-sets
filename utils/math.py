b882350427d7f9adb898088b9117590a66c62e1d
Bernardo Porto Veronese <bernardo.veronese@arcca.io>
2024-04-08 17:22:23 -0300

    Add solution for pb1-q6,q7

--- utils/math.py
+++ utils/math.py
@@ -1,6 +1,24 @@
 import numpy as np
 
-def gaussian(x: np.ndarray, mu: float, sigma: float):
-    norm = 1. / np.sqrt(2 * np.pi) * sigma
+def gaussian(x, mu: float, sigma: float):
+    norm = np.sqrt(2 * np.pi) * sigma
     chi_squared = (x - mu) ** 2 / sigma ** 2
-    return norm * np.exp(-0.5 * chi_squared)
+    return np.exp(-0.5 * chi_squared) / norm
+
+def loggaussian(x, mu: float, sigma: float):
+    norm = np.sqrt(2 * np.pi) * sigma
+    chi_squared = (x - mu) ** 2 / sigma ** 2
+    return -0.5 * chi_squared - np.log(norm)
+
+def uniform_prior(x, a, b):
+    return 1 / (b - a)
+
+def jeffreys_prior(x, a, b):
+    return 1 / x / np.log(b / a)
+
+def safe_exp(x):
+    xmax = np.max(x)
+    return np.exp(xmax) * np.exp(x - xmax)
+
+def normalize(y, x):
+    return y / np.trapz(y, x)
